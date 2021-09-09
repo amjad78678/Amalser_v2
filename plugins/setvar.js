5dbdfbba7b0705eab8dd6f59b41c7f82c622a4e4
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2021-09-10 01:45:07 +0530

    Update setvar.js

--- plugins/setvar.js
+++ plugins/setvar.js
@@ -18,7 +18,7 @@ Asena.addCommand({pattern: 'setvar', fromMe: false, desc: Lang.WP}, (async (mess
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━💘SETVAR💘━━⛦
-𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Amalser 𝐛𝐨𝐭.
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Amjad ser 𝐛𝐨𝐭.
   
 *ẉa.me/➳Setvar*
 ■□■□■□■□■□■□■□■□■□■□
@@ -90,15 +90,15 @@ All setvars are
  To get a song when your number is mentioned
  .setvar TAG_REPPLY:your jid 
 
-     *happy using Amalser bot ❣️*
+     *happy using Amjadser bot ❣️*
 
-*Editing And Devaloper : Amal -Amalser*
+*Editing And Devaloper : Amjad -Amjadali*
 
-*Erorr Fixing : Farhan -Julie Mwol 💌*
+*Erorr Fixing : Farhan 💌*
 
-*Supporting : Afnan -Pinky 💌*
+*Supporting : Amj 💌*
 
-*User Support : Adhithhari ✨️*
+*User Support : Power fans ✨️*
 
       ═════💢💢═════
 `}) 
